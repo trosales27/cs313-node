16a9e573cfdf8f0302d98efa97b32e1d7485d15e
Thomas Rosales <thomasrosales276@gmail.com>
2019-03-16 16:42:34 -0600

    starting project

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node index.js
+web: node muse_skill.js
---
+++ muse_skill.js
@@ -0,0 +1,26 @@
+var express = require("express");
+
+var app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.static("public"));
+
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+	console.log("Received a request for /");
+
+	res.write("This is the root.");
+	res.end();
+});
+
+app.get("/muse_skill", function(req, res) {
+    console.log("Received a request for muse_skill");
+
+    res.render("muse_skill");
+});
+
+app.listen(port, function() {
+	console.log("The server is up and listening on port 5000");
+});
